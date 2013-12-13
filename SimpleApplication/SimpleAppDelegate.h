4d53dfaf3f26ed9df9bfe516cfa81cd52fdc3a4c
masanori_msl <masuimasanori@masuimasanori-no-Mac-mini.local>
2013-12-13 20:13:31 +0900

    Initial Commit

---
+++ SimpleApplication/SimpleAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SimpleAppDelegate.h
+//  SimpleApplication
+//
+//  Created by masuimasanori on 2013/12/13.
+//  Copyright (c) 2013年 masanori. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SimpleAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SimpleApplication/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SimpleApplication
+//
+//  Created by masuimasanori on 2013/12/13.
+//  Copyright (c) 2013年 masanori. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SimpleAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SimpleAppDelegate class]));
+    }
+}
